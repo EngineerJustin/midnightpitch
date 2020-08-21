4f7a811e872727c8609ca694b6af707938a4f1d4
EngineerJustin <jrayjoseph@gmail.com>
2020-08-21 15:23:45 -0700

    Incorporating media

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,5 +1,46 @@
-import React from "react"
+import React, { Component } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import first from "../media/first.gif";
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default class SimpleSlider extends Component {
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <div>
+        <h2> Midnight City</h2>
+        <Slider {...settings}>
+          <div>
+              <img src={first} alt=" "/>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
+    );
+  }
 }
+
+// export default function Home() {
+//   return <div>Hello world!</div>
+// }
