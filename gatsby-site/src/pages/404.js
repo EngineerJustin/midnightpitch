36369b64a9a9beb2a92860bdf0127fe2e80733a0
EngineerJustin <jrayjoseph@gmail.com>
2020-08-28 12:34:15 -0700

    Switched to a for loop to populate slides to increase cleanliness of code

---
+++ gatsby-site/src/pages/404.js
@@ -0,0 +1,9 @@
+import React from "react"
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <h2>A Quantum Instability has torn this page from the fabric of space-time!</h2>
+    </div>
+  )
+}
--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -36,11 +36,19 @@ import ten from "../media/ten.jpg"
 export default class SimpleSlider extends Component {
   render() {
 
+    const titles = [one, two, three, four, five, six, seven, eight, nine, ten,
+    eleven, twelve, thirteen, fourteen, fifteen, sixteen, seventeen, eighteen,
+    nineteen, twenty, twentyone, twentytwo, twentythree, twentyfour, twentyfive,
+    twentysix, twentyseven, twentyeight];
+
+    const fill = [];
+
     const newContainer = {
       'max-width': "1500px",
     };
 
     const main = {
+      'maxWidth': "1500px",
       position: 'relative',
     };
 
@@ -57,214 +65,31 @@ export default class SimpleSlider extends Component {
                 arrows: true,
                 className: 'slides'
     };
+
+    for (let i = 0; i < 28; i+=1) {
+      fill.push(
+        <div style={newContainer} key={"slide" + i} className="container-fluid mt-5 mx-auto d-block eachSlide">
+            <div className="row">
+              <div className="col">
+                <img className="img-fluid mx-auto d-block" src={titles[i]} alt=" " />
+              </div>
+            </div>
+          </div>
+      )
+    };
+
     return (
-      <div style={newContainer, main} className="container-fluid" >
+      <div style={main} className="container-fluid" >
         <Slider {...settings} id="mainslide">
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="one">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={one} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="two">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={two} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="three">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={three} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="four">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={four} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="five">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={five} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="six">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={six} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="seven">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={seven} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="eight">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={eight} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="nine">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={nine} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="ten">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={ten} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="eleven">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={eleven} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="twelve">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={twelve} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="thirteen">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={thirteen} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="fourteen">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={fourteen} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="fifteen">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={fifteen} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="sixteen">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={sixteen} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="seventeen">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={seventeen} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="eighteen">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={eighteen} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="nineteen">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={nineteen} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="twenty">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={twenty} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="twentyone">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={twentyone} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="twentytwo">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={twentytwo} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="twentythree">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={twentythree} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="twentyfour">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={twentyfour} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="twentyfive">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={twentyfive} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="twentysix">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={twentysix} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="twentyseven">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={twentyseven} alt=" " />
-              </div>
-            </div>
-          </div>
-          <div style={newContainer} className="container-fluid mt-5 mx-auto d-block eachSlide" id="twentyeight">
-            <div className="row">
-              <div className="col">
-                <img className="img-fluid mx-auto d-block" src={twentyeight} alt=" " />
-              </div>
-            </div>
-          </div>
+          {fill}
         </Slider>
-        <div class="container mt-2">
-          <div class="row">
-            <div class="col text-center">
+        <div className="container mt-2">
+          <div className="row">
+            <div className="col text-center">
             <ReactAudioPlayer className="text-center"
               src={senseoftouch}
               controls
-              loop
-            />
+              loop/>
             </div>
           </div>
         </div>
